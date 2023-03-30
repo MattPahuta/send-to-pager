d3e78b5fef3f8324821e8fc6f70587a8fc1f7c64
Matt Pahuta <mhpahuta@gmail.com>
2023-03-30 14:17:49 -0700

    Added limit to page length, streamline styles and JS

--- app.js
+++ app.js
@@ -1,42 +1,37 @@
 // Send to Pager
 
-// Global variables
 const pagerContent = document.getElementById('pager-content');
 const phoneContent = document.getElementById('phone-content');
 const phone = document.getElementById('phone');
-const keypad = document.getElementById('keypad');
 pagerContent.textContent = '';
 phoneContent.textContent = '';
 
 phone.addEventListener('click', (e) => {
   // target keypad buttons
-  if (e.target.className === 'btn keypad-btn') {
-    console.log(e.target.textContent)
+  if (e.target.className === 'btn keypad-btn' && phoneContent.textContent.length < 15) {
     composePage(e.target.textContent)
   }
   // target reset button
   if (e.target.id === 'reset-btn') {
-    console.log('resetting display')
-    clearPhoneScreen()
+    phoneContent.textContent = '';
   }
   // target send button
   if (e.target.id === 'send-btn') {
-    console.log('sending pager...')
     sendPage()
   }
-
 })
 
-
-
+// compose page content
 function composePage(key) {
   phoneContent.textContent += key;
 }
 
+// send page
 function sendPage() {
-  pagerContent.textContent = phoneContent.textContent;
+  const audio = new Audio('./assets/pager.wav');
+  setTimeout(() => {
+    audio.play();
+    pagerContent.textContent = phoneContent.textContent;
+  }, 1500)
 }
 
-function clearPhoneScreen() {
-  phoneContent.textContent = '';
-}
